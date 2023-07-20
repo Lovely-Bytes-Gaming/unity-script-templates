b1200fdb54453d9f7cc449d8df55537bddcaf7ac
Nicholas Graetz <thegreatwormspirit@gmail.com>
2023-07-20 08:47:44 +0200

    added context menu to add or change namespace

--- Scripts/Editor/ScriptKeywordProcessor.cs
+++ Scripts/Editor/ScriptKeywordProcessor.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 internal sealed class ScriptKeywordProcessor : AssetModificationProcessor
 {
@@ -46,6 +47,90 @@ internal sealed class ScriptKeywordProcessor : AssetModificationProcessor
             OnWillMoveScriptAsset(sourcePath, destinationPath);
 
         return AssetMoveResult.DidNotMove;
+    }
+
+    [MenuItem("Assets/Set Namespace By Directory")]
+    private static void SetNamespaceByDirectory_ContextMenu() 
+    {
+        var obj = Selection.activeObject;
+        
+        if(!obj)
+            return;
+
+        var path = AssetDatabase.GetAssetPath(obj.GetInstanceID());
+        ProcessPath(path);
+    }
+
+    private static void ProcessPath(string path) 
+    {
+        if(Directory.Exists(path))
+            ProcessDirectory(path);
+
+        else if (path.EndsWith(".cs"))
+            ProcessScriptAsset(path);
+    }
+
+    private static void ProcessDirectory(string path) 
+    {
+        DirectoryInfo dirInfo = new DirectoryInfo(path);
+        var fileInfos = dirInfo.GetFiles();
+        var subdirs = dirInfo.GetDirectories();
+
+        foreach(var info in fileInfos)
+            ProcessPath(info.FullName);
+
+        foreach(var info in subdirs)
+            ProcessPath(info.FullName);
+    }
+
+    private static void ProcessScriptAsset(string path) 
+    {
+        path = path.Remove(0, path.IndexOf("Assets"));
+
+        var namespaceStr = "namespace " + NamespaceFromPath(path);
+        var scriptStr = File.ReadAllText(path);
+
+        if(!scriptStr.Contains("namespace "))
+            AddNamespaceKeyword(ref scriptStr);
+
+        int idx = 0;
+        while(true) 
+        {
+            idx = scriptStr.IndexOf("namespace ", idx);
+            
+            if(idx < 0)
+                break;
+
+            int endIdx = scriptStr.IndexOf('\n', idx);
+
+            scriptStr = scriptStr.Remove(idx, endIdx - idx);
+            scriptStr = scriptStr.Insert(idx, namespaceStr);
+
+            idx += "namespace ".Length;
+        }
+        System.IO.File.WriteAllText(path, scriptStr);
+    }
+
+    private static void AddNamespaceKeyword(ref string fileContent) 
+    {
+        int idx = fileContent.LastIndexOf("using");
+
+        if(idx < 0)
+            idx = 0;
+
+        else
+            idx = fileContent.IndexOf('\n', idx) + 1;
+
+        fileContent = fileContent.Insert(idx, "\nnamespace ReplaceMe\n{\n");
+        idx = fileContent.IndexOf('{') + 2;
+
+        do
+        {
+            fileContent = fileContent.Insert(idx, "\t");
+            idx = fileContent.IndexOf('\n', idx + 1) + 1;
+        } while (idx > 0);
+
+        fileContent += "\n}";
     }
 
     private static void OnWillMoveScriptAsset(string sourcePath, string destinationPath) 
